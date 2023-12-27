1fed86a24448da577f87e3a1712cf02c2cf86c6e
Julien Ripouteau <julien@ripouteau.com>
2023-12-28 00:28:50 +0100

    feat: initial commit

---
+++ packages/unilocks/bin/test.ts
@@ -0,0 +1,37 @@
+import 'dotenv/config'
+
+import { assert } from '@japa/assert'
+import { fileSystem } from '@japa/file-system'
+import { expectTypeOf } from '@japa/expect-type'
+import { processCLIArgs, configure, run } from '@japa/runner'
+
+import { BASE_URL } from '../test_helpers/index.js'
+
+/*
+|--------------------------------------------------------------------------
+| Configure tests
+|--------------------------------------------------------------------------
+|
+| The configure method accepts the configuration to configure the Japa
+| tests runner.
+|
+| The first method call "processCLIArgs" process the command line arguments
+| and turns them into a config object. Using this method is not mandatory.
+|
+| Please consult japa.dev/runner-config for the config docs.
+*/
+processCLIArgs(process.argv.slice(2))
+configure({
+  files: ['test/**/*.spec.ts'],
+  plugins: [assert(), expectTypeOf(), fileSystem({ autoClean: true, basePath: BASE_URL })],
+})
+
+/*
+|--------------------------------------------------------------------------
+| Run tests
+|--------------------------------------------------------------------------
+|
+| The following "run" method is required to execute all the tests.
+|
+*/
+run()
---
+++ packages/unilocks/test_helpers/index.ts
@@ -0,0 +1,1 @@
+export const BASE_URL = new URL('./tmp/', import.meta.url)
