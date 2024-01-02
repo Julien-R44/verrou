7a2857ffe6944b9889a344d24f009283e5e2ba19
Julien Ripouteau <julien@ripouteau.com>
2024-01-02 20:31:04 +0100

    chore: add playground

---
+++ playground/src/index.ts
@@ -0,0 +1,67 @@
+import { Verrou } from 'verrou'
+import { setTimeout } from 'node:timers/promises'
+import { redisStore } from 'verrou/drivers/redis'
+import { memoryStore } from 'verrou/drivers/memory'
+
+const verrou = new Verrou({
+  default: 'redis',
+  stores: {
+    memory: {
+      driver: memoryStore(),
+    },
+    redis: {
+      driver: redisStore({
+        connection: { host: 'localhost', port: 6379 },
+      }),
+    },
+  },
+})
+
+const products = [
+  { id: '123', name: 'Product A', stock: 10 },
+  { id: '456', name: 'Product B', stock: 5 },
+]
+
+/**
+ * Get a product by its ID.
+ * Simulate a database call by waiting 200ms
+ */
+async function getProduct(productId: string) {
+  await setTimeout(200)
+  return products.find((product) => product.id === productId)!
+}
+
+/**
+ * Update the stock of a product
+ */
+async function updateProductStock(productId: string, stock: number) {
+  const product = await getProduct(productId)
+  product.stock = stock
+}
+
+async function purchaseProduct(productId: string, quantity: number, customerId: string) {
+  const lockKey = `product-${productId}`
+
+  await verrou.createLock(lockKey).run(async () => {
+    const product = await getProduct(productId)
+
+    if (product.stock >= quantity) {
+      // Update the stock
+      await updateProductStock(productId, product.stock - quantity)
+
+      // Record the purchase
+      console.log(`Customer ${customerId} purchased ${quantity} of ${product.name}`)
+      console.log(`New stock for ${product.name} is ${product.stock}`)
+    } else {
+      console.log(`Not enough stock for ${product.name}. Purchase failed.`)
+    }
+  })
+}
+
+await Promise.all([
+  // Simulating two customers trying to purchase the same product at the same time
+  purchaseProduct('123', 1, 'CustomerA'),
+  purchaseProduct('123', 1, 'CustomerB'),
+])
+
+await verrou.disconnectAll()
